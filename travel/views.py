880c987a3121b3dd372174e0f99fd90833828116
Rishabh <n11494468@qut.edu.au>
2024-06-03 14:55:42 +1000

    modifies views.py to navigate

--- travel/views.py
+++ travel/views.py
@@ -1,13 +1,25 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from .models import Destination
 from . import db
+from flask import Blueprint, render_template
 
 mainbp = Blueprint('main', __name__)
 
 @mainbp.route('/')
 def index():
-    destinations = db.session.scalars(db.select(Destination)).all()    
-    return render_template('index.html', destinations=destinations)
+    return render_template('index.html')
+
+@mainbp.route('/create_event')
+def create_event():
+    return render_template('create_event.html')
+
+@mainbp.route('/event_details')
+def event_details():
+    return render_template('event_details.html')
+
+@mainbp.route('/bookings')
+def bookings():
+    return render_template('bookings.html')
 
 @mainbp.route('/search')
 def search():
