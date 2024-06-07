6d880b1904b350da7140fabe71862034ba82b35e
Rishabh <n11494468@qut.edu.au>
2024-06-07 14:26:33 +1000

    changes routes in views.py

--- travel/views.py
+++ travel/views.py
@@ -1,7 +1,9 @@
-from flask import Blueprint, render_template, request, redirect, url_for
-from .models import Event
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from .models import Event, Booking
 from . import db
-from flask import Blueprint, render_template
+from flask_login import current_user, login_required
+import random
+from datetime import datetime
 
 mainbp = Blueprint('main', __name__)
 
@@ -14,22 +16,43 @@ def index():
 def create_event():
     return render_template('create_event.html')
 
-@mainbp.route('/event_details')
-def event_details():
-    return render_template('event_details.html')
+@mainbp.route('/event_details/<int:event_id>')
+def event_details(event_id):
+    event = Event.query.get(event_id)
+    return render_template('event_details.html', event=event)
 
 @mainbp.route('/bookings')
+@login_required
 def bookings():
-    return render_template('bookings.html')
+    user_id = current_user.id
+    bookings = Booking.query.filter_by(userid=user_id).all()
+    return render_template('bookings.html', bookings=bookings)
 
 @mainbp.route('/search')
 def search():
-    if request.args['search'] and request.args['search'] != "":
-        print(request.args['search'])
+    if request.args.get('search'):
         query = "%" + request.args['search'] + "%"
         events = db.session.scalars(db.select(Event).where(Event.description.like(query)))
         return render_template('index.html', events=events)
     else:
         return redirect(url_for('main.index'))
 
+@mainbp.route('/book_event/<int:event_id>', methods=['POST'])
+@login_required
+def book_event(event_id):
+    number_of_tickets = request.form.get('number_of_tickets')
     
+    # Create a new booking with the current time as the booking_date
+    new_booking = Booking(
+        orderid=random.randint(1000000, 9999999),
+        userid=current_user.id,
+        event_id=event_id,
+        number_of_tickets=number_of_tickets,
+        booking_date=datetime.utcnow()
+    )
+    
+    # Add the new booking to the database
+    db.session.add(new_booking)
+    db.session.commit()
+    
+    return redirect(url_for('main.bookings'))
